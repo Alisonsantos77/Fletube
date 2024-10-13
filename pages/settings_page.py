b366b077b0696ad6bbc33d2d4a764e02a30e9659
Alisonsantos77 <Alisondev77@hotmail.com>
2024-10-13 18:34:02 -0300

    feat: Adicionar configurações detalhadas de downloads, aparência e gerais
    - Implementei as seções de configurações de downloads, aparência e gerais, cada uma com seus respectivos componentes.
    - Usei `SafeArea` para garantir que os elementos sejam exibidos corretamente em diferentes dispositivos.
    - Adicionei espaçamento e uma divisão clara entre as seções para melhorar a experiência do usuário.

--- pages/settings_page.py
+++ pages/settings_page.py
@@ -1,10 +1,29 @@
-import flet as ft 
+import flet as ft
+from components.download_settings import DownloadSettings
+from components.appearence_settings import AppearanceSettings
+from components.general_settings import GeneralSettings
+
 
 def SettingsPage(page: ft.Page):
-    return ft.Container(
+    download_settings = DownloadSettings(page)
+    appearance_settings = AppearanceSettings(page)
+    general_settings = GeneralSettings(page)
+
+    return ft.SafeArea(
         content=ft.Column(
-            [
-                ft.Text("Settings Page")
-            ]
-        )
+            controls=[
+                ft.Text("Configurações", size=30, weight=ft.FontWeight.BOLD),
+                ft.Divider(),
+                download_settings,
+                appearance_settings,
+                general_settings,
+            ],
+            spacing=20,
+            expand=True,
+        ),
+        expand=True,
+        top=True,
+        bottom=True,
+        left=False,
+        right=False,
     )
