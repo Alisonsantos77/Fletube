dc835af5004fb0fd36dad471b68f80617f5acc27
Alisonsantos77 <Alisondev77@hotmail.com>
2024-10-16 04:40:21 -0300

    - troca da paleta de cores

--- utils/theme.py
+++ utils/theme.py
@@ -1,33 +1,31 @@
 import flet as ft
 
 
-def DarkVideoQTheme():
+def BlueVibesDarkTheme():
     return ft.Theme(
         color_scheme=ft.ColorScheme(
-            primary="#ff0054",  # Cor principal vibrante
-            # Texto sobre a cor primária (branco para contraste)
-            on_primary="#fefeff",
-            background="#01002a",  # Cor de fundo escura
-            surface="#031867",  # Cor de superfícies (azul profundo)
-            # Texto sobre superfícies (branco para contraste)
-            on_surface="#fefeff",
-            secondary="#031867",  # Cor secundária (azul intenso)
-            on_secondary="#fefeff",  # Texto sobre a cor secundária
-            on_background="#fefeff",  # Texto sobre fundo escuro
+            primary="#0077B6",  # Cor principal vibrante (azul médio)
+            on_primary="#fefeff",  # Texto sobre a cor primária (branco para contraste)
+            background="#03045E",  # Cor de fundo escura (azul marinho profundo)
+            surface="#023E8A",  # Cor de superfícies (azul escuro)
+            on_surface="#fefeff",  # Texto sobre superfícies (branco para contraste)
+            secondary="#00B4D8",  # Cor secundária (azul claro)
+            on_secondary="#fefeff",  # Texto sobre a cor secundária (branco)
+            on_background="#fefeff",  # Texto sobre fundo (branco)
         )
     )
 
 
-def RelaxVideoQTheme():
+def BlueVibesLightTheme():
     return ft.Theme(
         color_scheme=ft.ColorScheme(
-            primary="#031867",  # Cor principal suave (azul profundo)
-            on_primary="#fefeff",  # Texto sobre a cor primária (branco)
-            background="#ff0054",  # Cor de fundo vibrante
-            surface="#01002a",  # Cor de superfícies (preto azulado)
-            on_surface="#fefeff",  # Texto sobre superfícies (branco)
-            secondary="#fefeff",  # Cor secundária (branco para contraste)
-            on_secondary="#031867",  # Texto sobre a cor secundária
-            on_background="#fefeff",  # Texto sobre fundo
+            primary="#0096C7",  # Cor principal suave (azul brilhante)
+            on_primary="#03045E",  # Texto sobre a cor primária (azul escuro para contraste)
+            background="#CAF0F8",  # Cor de fundo vibrante (azul muito claro)
+            surface="#ADE8F4",  # Cor de superfícies (azul claro)
+            on_surface="#03045E",  # Texto sobre superfícies (azul escuro)
+            secondary="#48CAE4",  # Cor secundária (azul aqua)
+            on_secondary="#03045E",  # Texto sobre a cor secundária (azul escuro)
+            on_background="#023E8A",  # Texto sobre fundo (azul escuro)
         )
     )
