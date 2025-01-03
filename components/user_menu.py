90135eb8df1ad589f273b158970d54e4a37dedfe
Alison Santos <Alisondev77@hotmail.com>
2025-01-03 15:01:04 -0300

    feat: melhorar menu de usuário com contador regressivo e logout aprimorado
    - Adicionado contador regressivo para exibir o tempo restante da assinatura do usuário.
    - Melhorado o processo de logout com `page.client_storage.clear()` para remover todas as informações armazenadas.
    - Substituído o acesso a variáveis de autenticação específicas por uma abordagem mais genérica e segura.
    - Melhorada a exibição das informações de assinatura e tempo restante diretamente no menu.
    - Implementada a classe `CountDown` para atualizar o tempo restante em tempo real.
    - Correção de cores e alinhamentos para melhor consistência visual.

--- components/user_menu.py
+++ components/user_menu.py
@@ -1,63 +1,94 @@
+import time
+import asyncio
 import flet as ft
-
-
-def logout_user(page: ft.Page):
-    # Remove as informações do usuário do client storage
-    page.client_storage.remove("user_avatar_url")
-    page.client_storage.remove("user_initials")
-    page.client_storage.remove("user_name")
-
-    # Remove as chaves de autenticação do GitHub e Google
-    page.client_storage.remove("github_token")
-    page.client_storage.remove("google_token")
-
-    page.go("/")
+from services.supabase_utils import user_is_active
+import logging
 
 
 def handle_user_action(action, page):
     if action == "Logout":
-        return logout_user(page)
+        try:
+            logging.info("Limpando clientStorage...")
+            page.client_storage.clear()
+            page.go("/login")
+        except Exception as e:
+            logging.error(f"Erro ao realizar logout: {e}")
     elif action == "Configurações":
         page.go("/configuracoes")
     elif action == "Perfil":
         pass
 
+# Contador regressivo para exibir o tempo restante da assinatura do usuário
+class CountDown(ft.Text):
+    def __init__(self, segundos):
+        super().__init__()
+        self.segundos = segundos
+
+    def did_mount(self):
+        self.executando = True
+        self.page.run_task(self.atualizar_timer)
+
+    def will_unmount(self):
+        self.executando = False
+
+    async def atualizar_timer(self):
+        while self.segundos and self.executando:
+            # Cálculo de dias, horas, minutos e segundos restantes
+            dias, resto_segundos = divmod(
+                self.segundos, 86400)  # 86400 segundos em um dia
+            horas, resto_segundos = divmod(
+                resto_segundos, 3600)  # 3600 segundos em uma hora
+            # 60 segundos em um minuto
+            minutos, segundos = divmod(resto_segundos, 60)
+
+            # Formatação para exibir como 'xd xh xm xs'
+            self.value = f"{dias}d {horas}h {minutos}m {segundos}s"
+            self.update()
+            await asyncio.sleep(1)
+            self.segundos -= 1            
+
 
 def create_user_menu(page: ft.Page):
-    avatar_url = page.client_storage.get("user_avatar_url")
-    user_initials = page.client_storage.get("user_initials") or "U"
-    user_name = page.client_storage.get("user_name") or "Usuário"
+    username = page.client_storage.get("username")
+    restantes = page.client_storage.get("dias_restantes")
+    assinatura = page.client_storage.get("subscription_type")
+
+    if restantes is None:
+        restantes = 0
+
+    # Iniciais do usuário
+    user_initials = username[0].upper() if username else "Fletube"
 
     return ft.PopupMenuButton(
         content=ft.Container(
             content=ft.Stack(
                 [
                     ft.CircleAvatar(
-                        foreground_image_src=avatar_url
-                        or "https://robohash.org/userdefault",
+                        foreground_image_src="https://robohash.org/{username}.png",
                         content=ft.Text(user_initials),
                     ),
                     ft.Container(
-                        content=ft.CircleAvatar(bgcolor=ft.colors.GREEN, radius=5),
+                        content=ft.CircleAvatar(
+                            bgcolor=ft.Colors.GREEN, radius=5),
                         alignment=ft.alignment.bottom_left,
                     ),
                 ],
                 width=40,
                 height=40,
             ),
-            tooltip=f"Logado como {user_name}",
+            tooltip=f"Logado como {username}",
         ),
         items=[
             ft.PopupMenuItem(
                 content=ft.Row(
                     controls=[
                         ft.Icon(
-                            name=ft.icons.PERSON_OUTLINE,
+                            name=ft.Icons.PERSON_4_SHARP,
                             size=20,
-                            color=ft.colors.BLUE_GREY_500,
+                            color=ft.Colors.BLUE_GREY_500,
                         ),
                         ft.Text(
-                            value="Perfil",
+                            value=f"{username}",
                             size=14,
                         ),
                     ]
@@ -68,9 +99,35 @@ def create_user_menu(page: ft.Page):
                 content=ft.Row(
                     controls=[
                         ft.Icon(
-                            name=ft.icons.SETTINGS,
+                            name=ft.Icons.CALENDAR_MONTH_ROUNDED,
                             size=20,
-                            color=ft.colors.BLUE_GREY_500,
+                            color=ft.Colors.BLUE_GREY_500,
+                        ),
+                        ft.Text(
+                            value=f"{assinatura}",
+                            size=14,
+                        ),]
+                ),
+            ),
+            ft.PopupMenuItem(
+                content=ft.Row(
+                    controls=[
+                        ft.Icon(
+                            name=ft.Icons.TIMER,
+                            size=20,
+                            color=ft.Colors.BLUE_GREY_500,
+                        ),
+                        CountDown(segundos=restantes * 86400) if restantes else ft.Text(value="Carregando...", size=14),
+                    ]
+                ),
+            ),
+            ft.PopupMenuItem(
+                content=ft.Row(
+                    controls=[
+                        ft.Icon(
+                            name=ft.Icons.SETTINGS,
+                            size=20,
+                            color=ft.Colors.BLUE_GREY_500,
                         ),
                         ft.Text(
                             value="Configurações",
@@ -80,14 +137,14 @@ def create_user_menu(page: ft.Page):
                 ),
                 on_click=lambda e: handle_user_action("Configurações", page),
             ),
-            ft.PopupMenuItem(),
+            ft.PopupMenuButton(),
             ft.PopupMenuItem(
                 content=ft.Row(
                     controls=[
                         ft.Icon(
-                            name=ft.icons.LOGOUT,
+                            name=ft.Icons.LOGOUT_SHARP,
                             size=20,
-                            color=ft.colors.RED,
+                            color=ft.Colors.RED,
                         ),
                         ft.Text(
                             value="Sair",
