6bb7927890ad471cc732b30c6b6f9f4815ebbec8
Alison Santos <Alisondev77@hotmail.com>
2025-01-05 20:43:39 -0300

    melhorias estéticas

--- partials/download_sidebar.py
+++ partials/download_sidebar.py
@@ -1,5 +1,3 @@
-# partials/download_sidebar.py
-
 import logging
 import flet as ft
 
@@ -8,6 +6,8 @@ logger = logging.getLogger(__name__)
 
 class SidebarList(ft.Container):
     def __init__(self, page: ft.Page):
+        self.page = page
+
         super().__init__(
             expand=True,
             padding=10,
@@ -15,8 +15,9 @@ class SidebarList(ft.Container):
             content=ft.Column(
                 controls=[
                     ft.Text(
-                        "🟢 Concluídos: 0 | 🔴 Falhas: 0",
-                        size=20,
+                        value=f"✅ Concluídos: {0} | ❌ Falhas: {
+                            0} | 📊 Total: {0} ",
+                        size=16,
                         weight=ft.FontWeight.BOLD,
                         color=ft.colors.BLUE_GREY_900,
                         text_align=ft.TextAlign.CENTER,
@@ -31,17 +32,32 @@ class SidebarList(ft.Container):
                         key="downloads_column",
                     ),
                 ],
+                alignment=ft.MainAxisAlignment.CENTER,
                 spacing=10,
                 expand=True,
             ),
             key="sidebar_container",
         )
+
         self.items = {}
         self.title_control = self.content.controls[0]
         self.downloads_column = self.content.controls[2]
-        self.page = page
+
         self.mounted = True
-        logger.info("SidebarList inicializado e montada.")
+        logger.info("SidebarList inicializado e montado.")
+
+        self.update_title_color()
+
+    def update_title_color(self):
+        """Atualiza a cor do título baseado no tema armazenado no client storage."""
+        if self.page:
+            theme_mode = self.page.client_storage.get("theme_mode", "light")
+            self.title_control.color = ft.colors.BLUE_700 if theme_mode == "light" else ft.colors.WHITE
+            self.title_control.animate_opacity = 500
+            self.title_control.update()
+        else:
+            logger.warning(
+                "self.page ainda não está inicializado. Adiando atualização da cor do título.")
 
     def on_unmount(self, e=None):
         """Callback quando o SidebarList é desmontado."""
@@ -49,48 +65,25 @@ class SidebarList(ft.Container):
         logger.info("SidebarList desmontado.")
 
     def add_download_item(self, id, title, subtitle, thumbnail_url, file_path):
-        """
-        Adiciona um novo item de download à barra lateral com animação de opacidade.
-        """
+        """Adiciona um novo item de download à barra lateral com animação de opacidade."""
         if not self.mounted:
-            print(
-                f"A SidebarList está tirando uma folga! Tentaremos adicionar o item '{title}' quando ela voltar ao trabalho."
-            )
+            print(f"A SidebarList está tirando uma folga! Tentaremos adicionar o item '{
+                  title}' quando ela voltar ao trabalho.")
             return
 
         # Criação inicial do item
         item = ft.ListTile(
             leading=ft.Container(
-                content=ft.Image(
-                    src=thumbnail_url,
-                    width=50,
-                    height=50,
-                    fit=ft.ImageFit.COVER,
-                ),
-                width=50,
-                height=50,
-                border_radius=ft.border_radius.all(5),
-                animate_opacity=500,
-                opacity=1,
+                content=ft.Image(src=thumbnail_url, width=50,
+                                 height=50, fit=ft.ImageFit.COVER),
+                width=50, height=50, border_radius=ft.border_radius.all(5), animate_opacity=500, opacity=1,
             ),
-            title=ft.Text(
-                value=title,
-                size=18,
-                weight=ft.FontWeight.BOLD,
-                color=ft.colors.BLUE_GREY_800,
-                max_lines=1,
-                overflow=ft.TextOverflow.ELLIPSIS,
-            ),
-            subtitle=ft.Text(
-                value=f"Formato: {subtitle}",
-                size=14,
-                color=ft.colors.BLUE_GREY_600,
-            ),
-            trailing=ft.Container(
-                content=ft.Icon(name=ft.icons.INFO, color=ft.colors.BLUE_500),
-                animate_opacity=500,
-                opacity=1,
-            ),
+            title=ft.Text(value=title, size=18, weight=ft.FontWeight.BOLD,
+                          color=ft.colors.LIGHT_BLUE_800, max_lines=1, overflow=ft.TextOverflow.ELLIPSIS),
+            subtitle=ft.Text(value=f"Formato: {
+                             subtitle}", size=14, color=ft.colors.LIGHT_BLUE_600),
+            trailing=ft.Container(content=ft.Icon(
+                name=ft.icons.INFO, color=ft.colors.BLUE_500), animate_opacity=500, opacity=1),
             data={"id": id, "status": "pending", "file_path": file_path},
             on_click=lambda e, id=id: self.on_item_click(id),
             animate_opacity=500,
@@ -103,24 +96,19 @@ class SidebarList(ft.Container):
 
         # Atualiza a contagem de downloads
         self.update_download_counts()
-        logger.info(
-            f"Item de download adicionado: ID: {id}, título: {title}, formato: {subtitle}, caminho: {file_path}"
-        )
+
+        logger.info(f"Item de download adicionado: ID: {id}, título: {
+                    title}, formato: {subtitle}, caminho: {file_path}")
 
     def on_item_click(self, id):
-        """
-        Função de callback quando um item de download é clicado.
-        """
+        """Função de callback quando um item de download é clicado."""
         logger.info(f"Item clicado: ID {id}")
 
     def update_download_item(self, id, progress, status):
-        """
-        Atualiza o estado de um item de download com animação.
-        """
+        """Atualiza o estado de um item de download com animação."""
         if not self.mounted:
-            print(
-                f"A SidebarList está ausente! O item '{id}' será atualizado na próxima oportunidade."
-            )
+            print(f"A SidebarList está ausente! O item '{
+                  id}' será atualizado na próxima oportunidade.")
             return
 
         item = self.items.get(id)
@@ -128,54 +116,57 @@ class SidebarList(ft.Container):
             try:
                 if status == "downloading":
                     item.trailing.content = ft.Text(
-                        f"{progress*100:.2f}%", size=14, color=ft.colors.BLUE_700
-                    )
+                        f"{progress*100:.2f}%", size=14, color=ft.colors.BLUE_700)
                     item.data["status"] = "downloading"
+                elif status == "converting":
+                    item.trailing.content = ft.Text(
+                        f"{progress*100:.2f}%", size=14, color=ft.colors.BLUE_700)
+                    item.data["status"] = "converting"
+                elif status == "pending":
+                    item.trailing.content = ft.Icon(
+                        name=ft.icons.INFO, color=ft.colors.BLUE_500)
+                    item.data["status"] = "pending"
+                    item.trailing.content.value = "📥 "
                 elif status == "finished":
                     item.trailing.content = ft.Icon(
-                        name=ft.icons.CHECK, color=ft.colors.GREEN
-                    )
+                        name=ft.icons.CHECK, color=ft.colors.GREEN)
                     item.data["status"] = "finished"
                 elif status == "error":
                     item.trailing.content = ft.Icon(
-                        name=ft.icons.ERROR, color=ft.colors.RED
-                    )
+                        name=ft.icons.ERROR, color=ft.colors.RED)
                     item.data["status"] = "error"
                 item.update()
 
                 self.update_download_counts()
             except Exception as e:
-                print(f"vish... deu ruim: {e} ")
+                print(f"vish... deu ruim: {e}")
         else:
-            print(
-                f"Oops! Parece que o item '{id}' ainda não foi adicionado à SidebarList."
-            )
+            print(f"Oops! Parece que o item '{
+                  id}' ainda não foi adicionado à SidebarList.")
 
     def update_download_counts(self):
-        """
-        Atualiza a contagem de downloads concluídos e com erro.
-        """
-        total = len(self.items)
-        errors = sum(
-            1 for item in self.items.values() if item.data.get("status") == "error"
-        )
-        finished = sum(
-            1 for item in self.items.values() if item.data.get("status") == "finished"
-        )
+        """Atualiza a contagem de downloads concluídos e com erro."""
 
-        self.title_control.value = f"🟢 Concluídos: {finished} | 🔴 Falhas: {errors}"
-        self.title_control.color = ft.colors.BLUE_GREY_900
+        total = len(self.items)
+
+        errors = sum(1 for item in self.items.values()
+                     if item.data.get("status") == "error")
+
+        finished = sum(1 for item in self.items.values()
+                       if item.data.get("status") == "finished")
+
+        # Atualiza o título com as contagens (finalizados, falhas e total)
+        self.title_control.value = f"✅ Concluídos: {
+            finished} | ❌ Falhas: {errors} | 📊 Total: {total}"
+        self.title_control.color = ft.colors.BLUE_700
         self.title_control.animate_opacity = 500
         self.title_control.update()
 
     def refresh_downloads(self, downloads):
-        """
-        Atualiza toda a lista de downloads com base no estado atual.
-        """
+        """Atualiza toda a lista de downloads com base no estado atual."""
         if not self.mounted:
             print(
-                "A SidebarList está tirando uma pausa. Vamos atualizar os downloads quando ela voltar!"
-            )
+                "A SidebarList está tirando uma pausa. Vamos atualizar os downloads quando ela voltar!")
             return
 
         try:
